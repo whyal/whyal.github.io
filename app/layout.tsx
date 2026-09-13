2ce7b95035b296b5ca2e06c1dd9f035c9676ce18
whyal <tydevsforfun@gmail.com>
2026-09-13 19:26:05 +0800

    fixed hydration warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
       suppressHydrationWarning
       className="h-full antialiased"
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col" suppressHydrationWarning>
         <Providers>{children}</Providers>
       </body>
     </html>
